57e22cd516141c5fea5be5c4670ffc787e9e2d94
luvisiki_99) <liuqianrong1323@gmail.com>
2024-07-12 18:46:28 +0800

    修改原作者当中的部分代码
    使用的时候发现定时推送流量当中发生问题，报错消息为：RuntimeWarning: coroutine 'Bot.send_message' was never awaited；
    因此将流量统计的位置进行部分重写，使用异步消息的方式来发送消息。

--- tg_bot.py
+++ tg_bot.py
@@ -1,6 +1,9 @@
 import json
 import os
 import telegram
+from telegram import Bot as TelegramBot
+from telegram.error import TelegramError
+import asyncio
 
 class Bot:
     def __init__(self):
@@ -8,14 +11,25 @@ class Bot:
         with open(r'./settings.json', 'r') as f:
             settings = json.loads(f.read())
         self.chat_id = settings['chat_id']
-        self.bot = telegram.Bot(token = settings['bot_token'])
+        self.bot = TelegramBot(token=settings['bot_token'])
 
-    def send_text(self, text):
-        return self.bot.send_message(chat_id = self.chat_id, text = text)
-    
-    def send_image(self, path):
-        with open(path, 'rb') as f:
-            return self.bot.send_photo(chat_id = self.chat_id, photo = f)
-    
-    def delete_msg(self, msg_id):
-        self.bot.delete_message(chat_id = self.chat_id, message_id = msg_id)
+    async def send_text(self, text):
+        try:
+            return await self.bot.send_message(chat_id=self.chat_id, text=text)
+        except TelegramError as e:
+            print(f"Failed to send text message: {e}")
+            return None
+
+    async def send_image(self, path):
+        try:
+            with open(path, 'rb') as f:
+                return await self.bot.send_photo(chat_id=self.chat_id, photo=f)
+        except TelegramError as e:
+            print(f"Failed to send image: {e}")
+            return None
+
+    async def delete_msg(self, msg_id):
+        try:
+            await self.bot.delete_message(chat_id=self.chat_id, message_id=msg_id)
+        except TelegramError as e:
+            print(f"Failed to delete message: {e}")
--- tg_stat.py
+++ tg_stat.py
@@ -1,40 +1,46 @@
 import os
-from tg_bot import Bot
 import json
+import asyncio
 from datetime import datetime
+from tg_bot import Bot
 
-os.chdir(os.path.dirname(__file__))
+async def main():
+    with open('settings.json', 'r') as f:
+        settings = json.loads(f.read())
+        servername = settings['servername']
+        total_traff = settings['total_traff_stat']
+        traff_methd = settings['traff_methd_stat']
+        ifce = settings['ifce_stat']
 
-with open('settings.json', 'r') as f:
-		settings = json.loads(f.read())
-		servername = settings['servername']
-		total_traff = settings['total_traff_stat']
-		traff_methd = settings['traff_methd_stat']
-		ifce = settings['ifce_stat']
+    bot = Bot()
 
-bot = Bot()
+    try:  # 以防文件不存在炸了
+        with open('./tg_stat_id.txt', 'r') as ids:
+            for id in ids:
+                id = int(id.strip())
+                await bot.delete_msg(id)
+    except FileNotFoundError:
+        pass
 
-try:		# 以防文件不存在炸了
-	with open('./tg_stat_id.txt', 'r') as ids:
-		for id in ids:
-			id = int(id.strip())
-			bot.delete_msg(id)
-except:
-	pass
+    os.system(f'vnstati -i {ifce} -m -o ./vnstat_m.png')
+    os.system(f'vnstati -i {ifce} -s -o ./vnstat_s.png')
 
-os.system(f'vnstati -i {ifce} -m -o ./vnstat_m.png')
-os.system(f'vnstati -i {ifce} -s -o ./vnstat_s.png')
+    nowdate = f"{datetime.now().year}年{datetime.now().month}月{datetime.now().day}日"
+    hello = f"{servername}\n节点总流量{total_traff}({traff_methd})\n今天是{nowdate}\n以下是当前流量统计信息："
 
-nowdate = str(datetime.now().year) + '年' + str(datetime.now().month) + '月' + str(datetime.now().day) + '日'
-hello = servername + '\n节点总流量' + total_traff + '(' + traff_methd + ')' + '\n今天是' + nowdate + '\n以下是当前流量统计信息：'
+    with open('./tg_stat_id.txt', 'w') as id_file:
+        res = await bot.send_text(hello)
+        if res:
+            id_file.write(f"{res.message_id}\n")
+        res = await bot.send_image('./vnstat_s.png')
+        if res:
+            id_file.write(f"{res.message_id}\n")
+        res = await bot.send_image('./vnstat_m.png')
+        if res:
+            id_file.write(f"{res.message_id}\n")
 
-with open('./tg_stat_id.txt', 'w') as id:
-	res = bot.send_text(hello)
-	id.write(str(res['message_id']) + '\n')
-	res = bot.send_image('./vnstat_s.png')
-	id.write(str(res['message_id']) + '\n')
-	res = bot.send_image('./vnstat_m.png')
-	id.write(str(res['message_id']) + '\n')
+    os.remove('./vnstat_m.png')
+    os.remove('./vnstat_s.png')
 
-os.system('rm -f ./vnstat_m.png')
-os.system('rm -f ./vnstat_s.png')
+if __name__ == "__main__":
+    asyncio.run(main())
